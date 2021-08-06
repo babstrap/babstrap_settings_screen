8c55e851e5045130013f219257ac6ea42a14592f
Babacar Ndong <ndongbabacar100@gmail.com>
2021-08-06 05:17:03 +0000

    Change the package usage example

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -15,95 +15,89 @@ class MyApp extends StatelessWidget {
       home: Scaffold(
         backgroundColor: Colors.white.withOpacity(.94),
         appBar: AppBar(
-          backgroundColor: Colors.blue,
           title: Text("Settings"),
           centerTitle: true,
         ),
         body: Padding(
           padding: const EdgeInsets.all(10),
-          child: Container(
-            height: MediaQuery.of(context).size.height,
-            child: ListView(
-              children: [
-                // User data section
-                // Settings groups sections
-                SettingsGroup(
-                  items: [
-                    SettingsItem(
-                      onTap: () {},
-                      icons: CupertinoIcons.pencil_outline,
-                      iconStyle: IconStyle(),
-                      title: 'Appearance',
-                      subtitle: "Make Ziar'App yours",
+          child: ListView(
+            children: [
+              SettingsGroup(
+                items: [
+                  SettingsItem(
+                    onTap: () {},
+                    icons: CupertinoIcons.pencil_outline,
+                    iconStyle: IconStyle(),
+                    title: 'Appearance',
+                    subtitle: "Make Ziar'App yours",
+                  ),
+                  SettingsItem(
+                    onTap: () {},
+                    icons: Icons.fingerprint,
+                    iconStyle: IconStyle(
+                      iconsColor: Colors.white,
+                      withBackground: true,
+                      backgroundColor: Colors.red,
                     ),
-                    SettingsItem(
-                      onTap: () {},
-                      icons: Icons.fingerprint,
-                      iconStyle: IconStyle(
-                        iconsColor: Colors.white,
-                        withBackground: true,
-                        backgroundColor: Colors.red,
-                      ),
-                      title: 'Privacy',
-                      subtitle: "Lock Ziar'App to improve your privacy",
+                    title: 'Privacy',
+                    subtitle: "Lock Ziar'App to improve your privacy",
+                  ),
+                  SettingsItem(
+                    onTap: () {},
+                    icons: Icons.dark_mode_rounded,
+                    iconStyle: IconStyle(
+                      iconsColor: Colors.white,
+                      withBackground: true,
+                      backgroundColor: Colors.red,
                     ),
-                    SettingsItem(
-                      onTap: () {},
-                      icons: Icons.dark_mode_rounded,
-                      iconStyle: IconStyle(
-                        iconsColor: Colors.white,
-                        withBackground: true,
-                        backgroundColor: Colors.red,
-                      ),
-                      title: 'Dark mode',
-                      subtitle: "Automatic",
-                      trailing: Switch.adaptive(
-                        value: false,
-                        onChanged: (value) {},
-                      ),
+                    title: 'Dark mode',
+                    subtitle: "Automatic",
+                    trailing: Switch.adaptive(
+                      value: false,
+                      onChanged: (value) {},
                     ),
-                  ],
-                ),
-                SettingsGroup(
-                  items: [
-                    SettingsItem(
-                      onTap: () {},
-                      icons: Icons.info_rounded,
-                      iconStyle: IconStyle(
-                        backgroundColor: Colors.purple,
-                      ),
-                      title: 'About',
-                      subtitle: "Learn more about Ziar'App",
+                  ),
+                ],
+              ),
+              SettingsGroup(
+                items: [
+                  SettingsItem(
+                    onTap: () {},
+                    icons: Icons.info_rounded,
+                    iconStyle: IconStyle(
+                      backgroundColor: Colors.purple,
                     ),
-                  ],
-                ),
-                // You can add a settings title
-                SettingsGroup(
-                  settingsGroupTitle: "Account",
-                  items: [
-                    SettingsItem(
-                      onTap: () {},
-                      icons: Icons.exit_to_app_rounded,
-                      title: "Sign Out",
+                    title: 'About',
+                    subtitle: "Learn more about Ziar'App",
+                  ),
+                ],
+              ),
+              // You can add a settings title
+              SettingsGroup(
+                settingsGroupTitle: "Account",
+                items: [
+                  SettingsItem(
+                    onTap: () {},
+                    icons: Icons.exit_to_app_rounded,
+                    title: "Sign Out",
+                  ),
+                  SettingsItem(
+                    onTap: () {},
+                    icons: CupertinoIcons.repeat,
+                    title: "Change email",
+                  ),
+                  SettingsItem(
+                    onTap: () {},
+                    icons: CupertinoIcons.delete_solid,
+                    title: "Delete account",
+                    titleStyle: TextStyle(
+                      color: Colors.red,
+                      fontWeight: FontWeight.bold,
                     ),
-                    SettingsItem(
-                      onTap: () {},
-                      icons: CupertinoIcons.repeat,
-                      title: "Change email",
-                    ),
-                    SettingsItem(
-                      onTap: () {},
-                      icons: CupertinoIcons.delete_solid,
-                      title: "Delete account",
-                      titleStyle: TextStyle(
-                        color: Colors.red,
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
-                  ],
-                ),
-              ],
-            ),
+                  ),
+                ],
+              ),
+            ],
           ),
         ),
       ),
