72041037e0afdaecc3c7831fbea33ec6f4461be9
Babacar Ndong <ndongbabacar100@gmail.com>
2021-08-06 04:24:24 +0000

    Add example

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,7 +1,6 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'dart:async';
-
-import 'package:flutter/services.dart';
+import 'package:babstrap_settings_screen/babstrap_settings_screen.dart';
 
 void main() {
   runApp(MyApp());
@@ -12,6 +11,102 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.white.withOpacity(.94),
+        appBar: AppBar(
+          backgroundColor: Colors.blue,
+          title: Text("Settings"),
+          centerTitle: true,
+        ),
+        body: Padding(
+          padding: const EdgeInsets.all(10),
+          child: Container(
+            height: MediaQuery.of(context).size.height,
+            child: ListView(
+              children: [
+                // User data section
+                // Settings groups sections
+                SettingsGroup(
+                  items: [
+                    SettingsItem(
+                      onTap: () {},
+                      icons: CupertinoIcons.pencil_outline,
+                      iconStyle: IconStyle(),
+                      title: 'Appearance',
+                      subtitle: "Make Ziar'App yours",
+                    ),
+                    SettingsItem(
+                      onTap: () {},
+                      icons: Icons.fingerprint,
+                      iconStyle: IconStyle(
+                        iconsColor: Colors.white,
+                        withBackground: true,
+                        backgroundColor: Colors.red,
+                      ),
+                      title: 'Privacy',
+                      subtitle: "Lock Ziar'App to improve your privacy",
+                    ),
+                    SettingsItem(
+                      onTap: () {},
+                      icons: Icons.dark_mode_rounded,
+                      iconStyle: IconStyle(
+                        iconsColor: Colors.white,
+                        withBackground: true,
+                        backgroundColor: Colors.red,
+                      ),
+                      title: 'Dark mode',
+                      subtitle: "Automatic",
+                      trailing: Switch.adaptive(
+                        value: false,
+                        onChanged: (value) {},
+                      ),
+                    ),
+                  ],
+                ),
+                SettingsGroup(
+                  items: [
+                    SettingsItem(
+                      onTap: () {},
+                      icons: Icons.info_rounded,
+                      iconStyle: IconStyle(
+                        backgroundColor: Colors.purple,
+                      ),
+                      title: 'About',
+                      subtitle: "Learn more about Ziar'App",
+                    ),
+                  ],
+                ),
+                // You can add a settings title
+                SettingsGroup(
+                  settingsGroupTitle: "Account",
+                  items: [
+                    SettingsItem(
+                      onTap: () {},
+                      icons: Icons.exit_to_app_rounded,
+                      title: "Sign Out",
+                    ),
+                    SettingsItem(
+                      onTap: () {},
+                      icons: CupertinoIcons.repeat,
+                      title: "Change email",
+                    ),
+                    SettingsItem(
+                      onTap: () {},
+                      icons: CupertinoIcons.delete_solid,
+                      title: "Delete account",
+                      titleStyle: TextStyle(
+                        color: Colors.red,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
