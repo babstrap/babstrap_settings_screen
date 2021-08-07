bc7338c037b06eb4326344aed3544a7485526451
Babacar Ndong <ndongbabacar100@gmail.com>
2021-08-07 17:47:43 +0000

    Adding the BigUserCard and the SmallUserCard

---
+++ lib/src/babs_component_big_user_card.dart
@@ -0,0 +1,102 @@
+import 'package:flutter/material.dart';
+
+class BigUserCard extends StatelessWidget {
+  Color? cardColor;
+  double? cardRadius;
+  Color? backgroundMotifColor;
+  Widget? cardActionWidget;
+  String? userName;
+  Widget? userMoreInfo;
+  ImageProvider userProfilePic;
+
+  BigUserCard({
+    required this.cardColor,
+    this.cardRadius = 30,
+    required this.userName,
+    this.backgroundMotifColor = Colors.white,
+    this.cardActionWidget,
+    this.userMoreInfo,
+    required this.userProfilePic,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    if (this.userMoreInfo == null) this.userMoreInfo = Container();
+    return Container(
+      height: MediaQuery.of(context).size.height / 4,
+      margin: EdgeInsets.only(bottom: 20),
+      decoration: BoxDecoration(
+        color: cardColor,
+        borderRadius:
+            BorderRadius.circular(double.parse(cardRadius!.toString())),
+      ),
+      child: Stack(
+        children: [
+          Align(
+            alignment: Alignment.bottomLeft,
+            child: CircleAvatar(
+              radius: 100,
+              backgroundColor: backgroundMotifColor!.withOpacity(.1),
+            ),
+          ),
+          Align(
+            alignment: Alignment.center,
+            child: CircleAvatar(
+              radius: 400,
+              backgroundColor: backgroundMotifColor!.withOpacity(.05),
+            ),
+          ),
+          Container(
+            margin: EdgeInsets.all(10),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisAlignment: (cardActionWidget != null)
+                  ? MainAxisAlignment.spaceEvenly
+                  : MainAxisAlignment.center,
+              children: [
+                Row(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    Expanded(
+                      child: CircleAvatar(
+                        radius: 50,
+                        backgroundImage: userProfilePic,
+                      ),
+                    ),
+                    Expanded(
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Text(
+                            userName!,
+                            style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontSize: 26,
+                              color: Colors.white,
+                            ),
+                          ),
+                          userMoreInfo!,
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+                Container(
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(50),
+                    color: Colors.white,
+                  ),
+                  child: (cardActionWidget != null)
+                      ? cardActionWidget
+                      : Container(),
+                )
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/src/babs_component_small_user_card copy.dart
@@ -0,0 +1,94 @@
+import 'package:flutter/material.dart';
+
+class SmallUserCard extends StatelessWidget {
+  Color? cardColor;
+  double? cardRadius;
+  Color? backgroundMotifColor;
+  VoidCallback? onTap;
+  String? userName;
+  Widget? userMoreInfo;
+  ImageProvider userProfilePic;
+
+  SmallUserCard({
+    required this.cardColor,
+    this.cardRadius = 30,
+    required this.userName,
+    this.backgroundMotifColor = Colors.white,
+    this.userMoreInfo,
+    required this.userProfilePic,
+    required this.onTap,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    if (this.userMoreInfo == null) this.userMoreInfo = Container();
+    return GestureDetector(
+      onTap: onTap,
+      child: Container(
+        height: MediaQuery.of(context).size.height / 6,
+        margin: EdgeInsets.only(bottom: 20),
+        decoration: BoxDecoration(
+          color: cardColor,
+          borderRadius:
+              BorderRadius.circular(double.parse(cardRadius!.toString())),
+        ),
+        child: Stack(
+          children: [
+            Align(
+              alignment: Alignment.bottomLeft,
+              child: CircleAvatar(
+                radius: 100,
+                backgroundColor: backgroundMotifColor!.withOpacity(.1),
+              ),
+            ),
+            Align(
+              alignment: Alignment.center,
+              child: CircleAvatar(
+                radius: 400,
+                backgroundColor: backgroundMotifColor!.withOpacity(.05),
+              ),
+            ),
+            Container(
+              margin: EdgeInsets.all(10),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Row(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      Expanded(
+                        child: CircleAvatar(
+                          radius: 50,
+                          backgroundImage: userProfilePic,
+                        ),
+                      ),
+                      Expanded(
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Text(
+                              userName!,
+                              style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                                fontSize: 26,
+                                color: Colors.white,
+                              ),
+                            ),
+                            userMoreInfo!,
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
