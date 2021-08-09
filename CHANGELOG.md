b97afe7a73aac02111173b8f1c2e895ac8229fcc
Babacar Ndong <ndongbabacar100@gmail.com>
2021-08-09 14:11:58 +0000

    Fix some Bugs

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -9,3 +9,7 @@
 ## 0.0.3
 
 * You can add the User data section using a BigUserCard or SmallUserCard widget.
+
+## 0.0.4
+
+* Fix Some Bugs
--- lib/babstrap_settings_screen.dart
+++ lib/babstrap_settings_screen.dart
@@ -9,3 +9,4 @@ export 'src/icon_style.dart';
 export 'src/babs_component_settings_item.dart';
 export 'src/babs_component_settings_group.dart';
 export 'src/babs_component_big_user_card.dart';
+export 'src/babs_component_small_user_card.dart';
