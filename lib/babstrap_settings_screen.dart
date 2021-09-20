c48e8d9884f4e799f3b06342a3a24e7c82ef79ea
Babacar Ndong <ndongbabacar100@gmail.com>
2021-09-20 14:04:40 +0000

    Fix the simpleUserCard BUG

--- lib/babstrap_settings_screen.dart
+++ lib/babstrap_settings_screen.dart
@@ -10,3 +10,4 @@ export 'src/babs_component_settings_item.dart';
 export 'src/babs_component_settings_group.dart';
 export 'src/babs_component_big_user_card.dart';
 export 'src/babs_component_small_user_card.dart';
+export 'src/babs_component_simple_user_card.dart';
